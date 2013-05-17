16877de2873134687294ae1e340dea575b5ce497
Sheroy <smarker@thoughtworks.com>
2013-05-16 20:22:56 -0700

    removed pngcrush for now

--- server.rb
+++ server.rb
@@ -13,11 +13,8 @@ def local_weather(latitude=37.5483, longitude=-121.9875)
   weather.fetch
   weather.process_svg
 
-  stdin, stdout, stderr = Open3.popen3("rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg")
+  stdin, stdout, stderr = Open3.popen3("rsvg-convert --background-color=white -o weather.png weather-script-output.svg")
   logger.info("rsvg-convert => #{stdin}, #{stdout}, #{stderr}")
-  
-  stdin, stdout, stderr = Open3.popen3("pngcrush -c0 w4 weather-script-output.png weather.png")
-  logger.info("pngcrush => #{stdin}, #{stdout}, #{stderr}")
   
   stdin, stdout, stderr = Open3.popen3("cp -f weather.png #{File.join(settings.public_folder, 'weather.png')}")
   logger.info( "cp => #{stdin}, #{stdout}, #{stderr}")
