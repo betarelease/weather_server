50d7205a055da9e4710b21790bdf6ed7a25a8909
Sheroy <smarker@thoughtworks.com>
2013-05-16 20:00:01 -0700

    fixed logging and basic request handling

--- server.rb
+++ server.rb
@@ -1,17 +1,24 @@
-Thread.new do
-  while true do
-     sleep 15
-     weather = Weather.new(37.5483, -121.9875)
-     weather.fetch
-     weather.process_svg
+get "/" do
+  "Welcome to the Weather App. You need to point your browser to /weather."
+end
 
-     stdin, stdout, stderr = Open3.popen3("rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg")
-     puts "[DEBUG] stdin, stdout, stderr => #{stdin}, #{stdout}, #{stderr}"
-     
-     stdin, stdout, stderr = Open3.popen3("pngcrush -c0 w4 weather-script-output.png weather.png")
-     puts "[DEBUG] stdin, stdout, stderr => #{stdin}, #{stdout}, #{stderr}"
-     
-     stdin, stdout, stderr = Open3.popen3("cp -f weather.png #{File.join(settings.public_folder, 'weather.png')}")
-     puts "[DEBUG] stdin, stdout, stderr => #{stdin}, #{stdout}, #{stderr}"
-  end
+get '/weather' do
+  local_weather
+  logger.info "#{File.join(settings.public_folder, 'weather.png')}"
+  send_file "#{File.join(settings.public_folder, 'weather.png')}"
+end
+
+def local_weather(latitude=37.5483, longitude=-121.9875)
+  weather = Weather.new(latitude, longitude)
+  weather.fetch
+  weather.process_svg
+
+  stdin, stdout, stderr = Open3.popen3("rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg")
+  logger.info("rsvg-convert => #{stdin}, #{stdout}, #{stderr}")
+  
+  stdin, stdout, stderr = Open3.popen3("pngcrush -c0 w4 weather-script-output.png weather.png")
+  logger.info("pngcrush => #{stdin}, #{stdout}, #{stderr}")
+  
+  stdin, stdout, stderr = Open3.popen3("cp -f weather.png #{File.join(settings.public_folder, 'weather.png')}")
+  logger.info( "cp => #{stdin}, #{stdout}, #{stderr}")
 end
