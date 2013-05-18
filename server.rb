f4f18985301341060a22375c0fd56e61de882a07
betarelease <sudhindra.r.rao@gmail.com>
2013-05-18 10:18:41 -0700

    adding pngcrush back since the image needs to fit kindle screen

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ end
 
 get '/weather' do
   local_weather
-  send_file "#{File.join(settings.public_folder, 'weather.png')}",
+  send_file "#{File.join(settings.public_folder, 'weather_small.png')}",
       :type => 'image/png',
       :disposition => 'inline'
 end
@@ -17,6 +17,9 @@ def local_weather(latitude=37.5483, longitude=-121.9875)
   stdin, stdout, stderr = Open3.popen3("rsvg-convert --background-color=white -o weather.png weather-script-output.svg")
   logger.info("rsvg-convert => #{stdin}, #{stdout}, #{stderr}")
   
-  stdin, stdout, stderr = Open3.popen3("mv -f weather.png #{settings.public_folder}")
+  stdin, stdout, stderr = Open3.popen3("pngcrush -c0 w4 weather.png weather_small.png")
+  logger.info("pngcrush => #{stdin}, #{stdout}, #{stderr}")
+  
+  stdin, stdout, stderr = Open3.popen3("mv -f weather_small.png #{settings.public_folder}")
   logger.info( "cp => #{stdin}, #{stdout}, #{stderr}")
 end
