b6c7e503f5b1f6ef15fcd2e42abca6bd1ec3e4d0
betarelease <sudhindra.r.rao@gmail.com>
2013-05-20 20:40:02 -0700

    constantized output filename

--- server.rb
+++ server.rb
@@ -1,10 +1,12 @@
+WEATHER_DASHBOARD=File.join(settings.public_folder, 'weather.png')
+
 get "/" do
   "Welcome to the Weather App. You need to point your browser to /weather."
 end
 
 get '/weather' do
   local_weather
-  send_file "#{File.join(settings.public_folder, 'weather_small.png')}",
+  send_file WEATHER_DASHBOARD,
       :type => 'image/png',
       :disposition => 'inline'
 end
@@ -15,5 +17,5 @@ def local_weather(latitude=37.5483, longitude=-121.9875)
   weather.process_svg
 
   `rsvg-convert --background-color=white -o weather.png weather-script-output.svg > rsvg-convert.log 2>&1`  
-  `pngcrush -c0 w4 weather.png weather_small.png > pngcrush.log 2>&1`
+  `pngcrush -c0 w4 weather.png #{WEATHER_DASHBOARD} > pngcrush.log 2>&1`
 end
