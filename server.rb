025559bb996f2e7af0cac60ffe9a06b68c9f48da
betarelease <sudhindra.r.rao@gmail.com>
2013-05-20 20:31:45 -0700

    redirecting to logfile instead of using open3

--- server.rb
+++ server.rb
@@ -14,12 +14,6 @@ def local_weather(latitude=37.5483, longitude=-121.9875)
   weather.fetch
   weather.process_svg
 
-  stdin, stdout, stderr = Open3.popen3("rsvg-convert --background-color=white -o weather.png weather-script-output.svg")
-  logger.info("rsvg-convert => #{stdin}, #{stdout}, #{stderr}")
-  
-  stdin, stdout, stderr = Open3.popen3("pngcrush -c0 w4 weather.png weather_small.png")
-  logger.info("pngcrush => #{stdin}, #{stdout}, #{stderr}")
-  
-  stdin, stdout, stderr = Open3.popen3("mv -f weather_small.png #{settings.public_folder}")
-  logger.info( "cp => #{stdin}, #{stdout}, #{stderr}")
+  `rsvg-convert --background-color=white -o weather.png weather-script-output.svg > rsvg-convert.log 2>&1`  
+  `pngcrush -c0 w4 weather.png weather_small.png > pngcrush.log 2>&1`
 end
